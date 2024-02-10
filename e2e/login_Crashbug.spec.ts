cfb4fa92719f0c2702346b96b6dbeedcc46648e8
QAPro <phil.bailey001@gmail.com>
2024-02-09 16:01:15 -0800

    troubleshooting first login test after reconfig

--- e2e/login_Crashbug.spec.ts
+++ e2e/login_Crashbug.spec.ts
@@ -1,14 +1,14 @@
-import { test, expect, } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('test', ({ page }) => {
+test('test', async ({ page }) => {
   await page.goto('https://n.rivals-staging.com/');
   await page.getByText('Log in').click();
   await page.getByTestId('login-form-username-or-email-input').click();
   await page.getByTestId('login-form-username-or-email-input').fill('crashbug');
   await page.getByTestId('login-form-password-input').click();
   await page.getByTestId('login-form-password-input').fill('Testing1!');
-  await expect (page.getByTestId('login-form-username-or-email-input')).toContainText('crashbug')
-  await expect (page.getByTestId('login-form-password-input')).toContainText('Testing1!')
+  await expect(page.getByTestId('login-form-username-or-email-input')).toHaveValue('crashbug');
+  await expect (page.getByTestId('login-form-password-input')).toHaveValue('Testing1!');
   await page.getByTestId('login-form-submit-button').click();
   await expect(page.locator('.site-header')).toBeVisible();
   await expect(page.getByRole('button')).toContainText('crashbug');
--- playwright.config.ts
+++ playwright.config.ts
@@ -10,11 +10,12 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  timeout: 60000,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
